31ba4ed7708b7e4c0a38e35429cfb3bd8a1f1ed4
Mikkel Gravgaard <mikkel@klokke.dk>
2016-12-03 20:09:37 +0100

    description and github page url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject reading-list "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Export Safari's reading list as html"
+  :url "http://github.com/grav/reading-list"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
